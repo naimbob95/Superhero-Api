944707228ea77623bffd44373943db7b5da45bfc
naimbob95 <naimbob95@gmail.com>
2019-05-25 16:34:06 +0800

    aaa

--- create_db.php
+++ create_db.php
@@ -1,11 +1,11 @@
 <?php
 	echo "<br />Creating db now....";
 
-	// mysql://b31145a0578643:10c62e01@us-cdbr-iron-east-02.cleardb.net/heroku_5795f41579d14c9?reconnect=true
-   $dbhost="us-cdbr-iron-east-02.cleardb.net";
-   $dbuser="b31145a0578643";
-   $dbpass="10c62e01";
-   $dbname="heroku_5795f41579d14c9";	
+	//mysql://b2c04d13ed6b4c:40729ffe@us-cdbr-iron-east-02.cleardb.net/heroku_b87ec2784b3ca40?reconnect=true
+	$dbhost="us-cdbr-iron-east-02.cleardb.net";
+	$dbuser="b31145a0578643";
+	$dbpass="10c62e01"; 
+	$dbname="heroku_5795f41579d14c9";	
 
    $db = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -14,17 +14,15 @@
 
    //*
    try {
-		$sql_create_applications_tbl = <<<EOSQL
-			CREATE TABLE IF NOT EXISTS applications (
+		$sql_create_contacts_tbl = <<<EOSQL
+			CREATE TABLE IF NOT EXISTS contacts (
 			  id int(11) NOT NULL AUTO_INCREMENT,
 			  name varchar(150) NOT NULL,
-			  cape varchar(10) NOT NULL,
-			  mask varchar(10) NOT NULL,
-			  costume varchar(10) NOT NULL,
-			  superpower varchar(50) NOT NULL,
+			  email varchar(250) NOT NULL,
+			  mobileno varchar(15) NOT NULL,
+			  photo varchar(150) NOT NULL DEFAULT 'default.png',
 			  ownerlogin varchar(50) NOT NULL,
 			  addeddate datetime NOT NULL,
-			  verify int(11) NOT NULL,
 			  status int(11) NOT NULL DEFAULT '0',
 			  PRIMARY KEY (id)
 			) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1
@@ -77,7 +75,7 @@
    //*
    try {
 		$sql_insert_user_into_users = <<<EOSQL
-			INSERT INTO users (login, password, name, email, mobileno, photo, addeddate) VALUES ('admin', 'b42a6d93d7969152e0f18f0e41c0f4f2bc9625f06c43dcbc22f6ffb2ffdd6137d93c1cdbb16', 'admin', 'admin@gmail.com', '0123456789', 'default.png', NOW());
+			INSERT INTO users (login, password, name, email, mobileno, photo, addeddate) VALUES ('baba', 'b42a6d93d7969152e0f18f0e41c0f4f2bc9625f06c43dcbc22f6ffb2ffdd6137d93c1cdbb16', 'ali', 'ali@gmail.com', '0123456789', 'default.png', NOW());
 		EOSQL;
 
 		$result = $db->exec($sql_insert_user_into_users); 	
@@ -95,4 +93,6 @@
    //*/    
 
      
+
+
 
